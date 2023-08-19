f4c45b2082b97ea70ca3d3fd3e113b9f3b8c427b
booi-dev <booimangang@gmail.com>
2023-08-19 23:06:45 +0530

    Adding multiple circles

--- script.js
+++ script.js
@@ -23,9 +23,24 @@ ctx.lineTo(250, 400);
 ctx.strokeStyle = "white";
 ctx.stroke();
 
+// arc
 // Draw the circle
 ctx.beginPath();
-ctx.arc(200, 600, 60, 0, Math.PI * 2);
+ctx.arc(200, 300, 60, 0, Math.PI * 2, false);
 ctx.fillStyle = "white";
 ctx.fill();
 ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(400, 300, 60, 0, Math.PI * 2, false);
+ctx.fillStyle = "white";
+ctx.stroke();
+ctx.closePath();
+
+for (let i = 0; i < 10; i++) {
+  ctx.beginPath();
+  ctx.arc(200 + i * 100, 500, 30, 0, Math.PI * 2, false);
+  ctx.fillStyle = "white";
+  ctx.stroke();
+  ctx.closePath();
+}
