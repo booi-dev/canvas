18077ce260631e4a772e93984d9ececc1b2f2419
booi-dev <booimangang@gmail.com>
2023-08-20 00:54:08 +0530

    Create 100 random circles

--- move/script.js
+++ move/script.js
@@ -7,34 +7,56 @@ const ctx = canvas.getContext("2d");
 
 ///
 
-let circleRadius = 50;
+function getRandomColor() {
+  // Generate random values for red, green, and blue channels
+  const r = Math.floor(Math.random() * 256); // 0 to 255
+  const g = Math.floor(Math.random() * 256); // 0 to 255
+  const b = Math.floor(Math.random() * 256); // 0 to 255
 
-const drawCircle = (startPosX, startPosY) => {
-  ctx.beginPath();
-  ctx.arc(startPosX, startPosY, circleRadius, 0, Math.PI * 2, false);
-  ctx.fillStyle = "#D9C5B2";
-  ctx.fill();
-  ctx.closePath();
+  // Create a CSS color string in the format "rgb(r, g, b)"
+  const color = `rgb(${r}, ${g}, ${b})`;
+
+  return color;
+}
+const circle = (x, y, r = 50) => {
+  let circleRadius = Math.random() * (10 + 40) + 10;
+
+  let dx = (Math.random() - 0.5) * 10;
+  let dy = (Math.random() - 0.5) * 10;
+
+  const drawCircle = () => {
+    if (x + circleRadius > window.innerWidth || x - circleRadius < 0) dx = -dx;
+    if (y + circleRadius > window.innerHeight || y - circleRadius < 0) dy = -dy;
+
+    x += dx;
+    y += dy;
+
+    ctx.beginPath();
+    ctx.arc(x, y, circleRadius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#D9C5B2";
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  return drawCircle;
 };
 
-let startX = 200;
-let startY = 200;
-let dx = 10;
-let dy = 10;
+const genRandX = () => Math.random() * window.innerWidth;
+const genRandY = () => Math.random() * window.innerHeight;
+
+const circles = Array.from({ length: 100 }, () => {
+  const startX = genRandX();
+  const startY = genRandY();
+  return circle(startX, startY);
+});
 
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  drawCircle(startX, startY);
 
-  if (startX + circleRadius > window.innerWidth || startX - circleRadius === 0)
-    dx = -dx;
-
-  if (startY + circleRadius > window.innerHeight || startY - circleRadius === 0)
-    dy = -dy;
-
-  startX += dx;
-  startY += dy;
+  circles.forEach((drawCircle) => {
+    drawCircle();
+  });
 };
 
 animate();
